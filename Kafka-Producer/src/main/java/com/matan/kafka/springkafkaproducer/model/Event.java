e73fcab9e293d7735e7df08857603b46de003bde
matan <matan@matan>
2021-03-02 11:32:24 +0200

    .

--- Kafka-Producer/src/main/java/com/matan/kafka/springkafkaproducer/model/Event.java
+++ Kafka-Producer/src/main/java/com/matan/kafka/springkafkaproducer/model/Event.java
@@ -1,5 +1,6 @@
 package com.matan.kafka.springkafkaproducer.model;
 
+import java.io.Serializable;
 import java.util.Date;
 import org.springframework.data.annotation.Id;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -10,8 +11,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Event {
+public class Event implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	@Id
 	private int reporterId;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
